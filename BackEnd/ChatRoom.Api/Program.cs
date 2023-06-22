3867b2506c0fa34b0cf458db9f08c0bd42a68eb0
Pablo Uribe <pablo.um00@gmail.com>
2023-06-22 12:55:06 -0500

    setup for running it in my local network

--- BackEnd/ChatRoom.Api/Program.cs
+++ BackEnd/ChatRoom.Api/Program.cs
@@ -7,10 +7,14 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "AllowSpecificOrigins",
         policy =>
         {
-            policy.WithOrigins(builder.Configuration.GetSection("AppUrlFE").Value)
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-                .AllowCredentials();
+            var urls = builder.Configuration.GetSection("AppUrlFE").Get<string[]>();
+            foreach (var appUrl in urls)
+            {
+                policy.WithOrigins(appUrl)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            }
         });
 });
 
